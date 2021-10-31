dcf556da9d2b8914378ba2dd5ff9df6f0d3ce120
Julian Trachsel <julestrachsel@gmail.com>
2021-10-31 18:33:51 +0000

    new funct doc

--- R/pangenome_select_representative_set.R
+++ R/pangenome_select_representative_set.R
@@ -1,4 +1,14 @@
 
+#' Generate a genome tibble containing presence/absence of genes in a fake pangenome
+#'
+#' @param genome_name name of the genome to generate
+#' @param num_genes number of genes the genome should contain
+#' @param core_genome_fraction fraction of the genome that is part of the 'core pangenome'
+#'
+#' @return 3 column tibble 1) genome_name; 2) gene_name 3) gene_presence
+#' @export
+#'
+#' @examples #soon
 generate_genome_vector <- function(genome_name, num_genes, core_genome_fraction=.75){
 
   core_genome_size= floor(num_genes * core_genome_fraction)
@@ -26,6 +36,17 @@ generate_genome_vector <- function(genome_name, num_genes, core_genome_fraction=
 }
 # generate_genome_vector(genome_name = 'genome_1', num_genes = 5000)
 
+#' Generate a random synthetic pangenome gene presence absence matrix
+#'
+#' @param num_genomes number of genomes the matrix will contain
+#'
+#' @param num_genes number of genes the matrix will contain
+#' @param core_genome_fraction fraction of genes that are part of the core genome
+#'
+#' @return gene presence absence matrix (0/1), rows are genomes, columns are genes
+#' @export
+#'
+#' @examples #soon
 generate_pangenome <- function(num_genomes=100, num_genes=1000, core_genome_fraction=.75){
   genomes <- paste0('genome_', 1:num_genomes)
   pangenome_matrix <-
@@ -55,6 +76,15 @@ generate_pangenome <- function(num_genomes=100, num_genes=1000, core_genome_frac
 # desired_coverage=.95
 # desired_score <- best_score * desired_coverage
 
+#' return the coverage score (proportion coverage) of a random set of genomes
+#'
+#' @param pan_mat input pangenome PA matrix
+#' @param set_size number of genomes to randomly select
+#'
+#' @return returns a named list of length 2, [[1]] = scores, [[2]] genome indicies
+#' @export
+#'
+#' @examples #soon
 return_set_score <- function(pan_mat, set_size){
   #subset a pangenome to a random collection of a defined size
   # return a score that describes the proportion of pangenomes total gene content
@@ -68,6 +98,17 @@ return_set_score <- function(pan_mat, set_size){
   return(list(score=score, set_indicies=set_indicies))
 }
 
+#' look for small sets of pangenomes that produce the desired gene content coverage of a pangenome
+#'
+#' @param pan_mat input pangenome PA matrix
+#' @param desired_coverage proportion of the pangenome to cover (.95)
+#' @param starting_set_size initial number of genomes to use as a set size
+#' @param num_iters_per_size number of samples to take for each set size setp
+#'
+#' @return tibble of sets of genomes that meet the desired coverage threshold, will be at least one set, but maybe more.
+#' @export
+#'
+#' @examples
 get_gene_content_reps <- function(pan_mat, desired_coverage=.95, starting_set_size=1, num_iters_per_size=500){
   # hopefully get smallest set of genomes that gives desired coverage of pangenome
   # browser()
