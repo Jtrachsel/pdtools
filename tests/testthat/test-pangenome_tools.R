281c7f238e7022bee16986f93fc88a52524de1c6
jtrachsel <julestrachsel@gmail.com>
2021-11-11 13:50:32 -0600

    add tests for pangenome tools

---
+++ tests/testthat/test-pangenome_tools.R
@@ -0,0 +1,24 @@
+
+test_that('generate_genome_vector("genome_1", 20) returns an appropriate tibble',{
+  genome <- generate_genome_vector('genome_1', 20)
+  expect_equal(dim(genome), c(20,3))
+})
+
+test_that('generate_pangenome() returns an appropriate matrix',{
+  expect_equal(dim(generate_pangenome(num_genomes = 3, num_genes = 20)), c(3,20))
+})
+
+
+test_that('build_ppanggolin_file_fastas() returns an appropriate tibble',{
+  expect_equal(dim(build_ppanggolin_file_fastas(incomplete_genome_paths = './test.fa')), c(1,3))
+})
+
+test_that('pan_mat_to_gene_vec_tibble() returns an appropriate tibble',{
+  expect_equal(dim(pan_mat_to_gene_vec_tibble(generate_pangenome())), c(100,2))
+})
+
+test_that('get_pangenome_representatives() returns an appropriate list',{
+  test <- get_pangenome_representatives(pan_mat = generate_pangenome(), desired_coverage = 1)
+  expect_equal(lapply(test, typeof) |> unlist(), c('character', 'integer', 'double'))
+})
+
