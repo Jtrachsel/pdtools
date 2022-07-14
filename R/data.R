9ec881932ac472b66dab851ab0a4fb9ca3646f95
Julian Trachsel <julestrachsel@gmail.com>
2022-07-14 21:34:40 +0000

    Update example data, add example pangenome

--- R/data.R
+++ R/data.R
@@ -6,7 +6,7 @@
 #' @format A data frame with 200 rows and 64 variables:
 #'
 #'
-#' @source \url{https://ftp.ncbi.nlm.nih.gov/pathogen/Results/Klebsiella/PDG000000012.1049/AMR/PDG000000012.1049.amr.metadata.tsv}
+#' @source \url{https://ftp.ncbi.nlm.nih.gov/pathogen/Results/Klebsiella/}
 "klebsiella_example_dat"
 
 
@@ -22,3 +22,27 @@
 #'
 #' @source the maps package
 "country_vector"
+
+#' A gene presence absence matrix for a synthetic pangenome
+#'
+#' Used for pangenome function examples
+#'
+#'
+#' @format A presence/absence matrix with genes as rows and genomes as columns
+#'
+#'
+#' @source pdtools:::generate_pangenome()
+"example_pangenome_matrix"
+
+#' A distance matrix describing the jaccard distance between genomes in the
+#' provided example pangenome
+#'
+#' Used for pangenome function examples
+#'
+#'
+#' @format A dist object
+#'
+#'
+#' @source dist(method = 'binary', t(example_pangenome_matrix))
+
+"example_pan_dist"
